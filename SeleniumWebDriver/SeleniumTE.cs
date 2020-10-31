a536bd6b75935361a1e979840457f1a210a35dda
Yidnekachew Kibru <yidnekibru@gmail.com>
2020-10-31 16:44:44 +0300

    Updated url

--- SeleniumWebDriver/SeleniumTE.cs
+++ SeleniumWebDriver/SeleniumTE.cs
@@ -17,7 +17,6 @@ namespace SeleniumWebDriver
         [SetUp]
         public void startBrowser()
         {
-            
             driver = new ChromeDriver("C://Users/yidne/Downloads/Compressed/chromedriver_win32");
         }
 
